12d8d4d3168cba3bb4d6f5f870df7bf23301f9c1
ritesh-kr18 <riteshsonb18@gmail.com>
2026-09-01 21:28:42 +0530

    Fix st.image TypeError

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ else:
     if uploaded_file is not None:
         # Display the uploaded image
         image = Image.open(uploaded_file).convert('RGB')
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_container_width=True)
         
         st.write("Processing...")
         
